1cbc5395d425359a13869fe6a3615d879bc5f0d8
Gabriel <gcamargo.dev@gmail.com>
2022-11-08 22:46:58 -0300

    🔄 refactor: refactored Movie List component to avoid code reuse

--- src/pages/Trending/index.tsx
+++ src/pages/Trending/index.tsx
@@ -2,16 +2,16 @@ import { Wrapper } from "../../components/common/Wrapper";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import TrendingContent from "../../components/TrendingContent";
 
 export default function Trending() {
 	return (
 		<>
 			<Header />
-      
 			<Wrapper>
 				<Sidebar />
+				<TrendingContent />
 			</Wrapper>
-
 			<Footer />
 		</>
 	);
