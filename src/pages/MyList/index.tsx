743fee4046d4c180cb45753e9549256a39c53159
Gabriel <gcamargo.dev@gmail.com>
2022-11-11 13:10:41 -0300

    🔥feat: created condition to show my list only when user is logged

--- src/pages/MyList/index.tsx
+++ src/pages/MyList/index.tsx
@@ -1,10 +1,26 @@
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
 import MyListContent from "../../components/MyListContent";
 import PageWrapper from "../../components/PageWrapper";
+import { Container } from "./styles";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/common/Button";
 
 export default function MyList() {
+	const { isUserLogged } = useContext(UserContext);
+
 	return (
 		<PageWrapper>
-			<MyListContent />
+			{!isUserLogged && (
+				<Container>
+					<h1>You must be logged to add movies to your list.</h1>
+
+					<Link to="/user">
+						<Button>Log in</Button>
+					</Link>
+				</Container>
+			)}
+			{isUserLogged && <MyListContent />}
 		</PageWrapper>
 	);
 }
---
+++ src/pages/MyList/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  max-width: 750px;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+
+  a {
+    width: 100%;
+  }
+`;
